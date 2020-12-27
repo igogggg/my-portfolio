c0e8c00c91961e5dbb2c2f6664f395de1b4b9545
igogggg <64080406+igogggg@users.noreply.github.com>
2020-12-27 19:23:04 +0300

    finish loading

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -1,39 +1,65 @@
 import React from "react"
 import styled from "styled-components"
-import { motion } from "framer-motion"
+import { keyframes } from "styled-components"
+
+const symbolAnim = keyframes`
+0%{
+  stroke-dashoffset: 600;
+}
+
+100%{
+  stroke-dashoffset: 0;
+  
+}
+`
+const dotAnim = keyframes`
+0%{
+  stroke-dashoffset:465; 
+  
+};
+50%{
+  stroke-dashoffset:465;
+  
+}
+
+100%{
+  stroke-dashoffset:0;
+  
+};
+`
+const paper = keyframes`
+0%{
+  transform:scale(1)
+}
+75%{
+  transform:scale(1)
+}
+100%{
+  transform:scale(0) rotate(360deg)
+}
+`
 
 const LoadingBackground = styled.div`
-  background-color: #121d2f;
+  background-color: var(--blue-black);
+  min-width: 100vw;
+  min-height: 100vh;
   display: grid;
   place-content: center;
+  .symbol {
+    stroke-dasharray: 600;
+    stroke-dashoffset: 600;
+    animation: ${symbolAnim} 2s linear forwards;
+  }
+  .dot {
+    stroke-dasharray: 465;
+    stroke-dashoffset: 465;
+    animation: ${dotAnim} 3s linear forwards;
+  }
+  #miLogo {
+    animation: ${paper} 4.5s linear forwards;
+  }
 `
-// const svgVariant = {
-//   hidden: {
-//     rotate: -180,
-//   },
-//   visible: {
-//     rotate: 0,
-//     transition: {
-//       duration: 1,
-//     },
-//   },
-// }
-// const pathVariants = {
-//   hidden: {
-//     pathLenght: 1,
-//     pathOffset: 0,
-//     opacity: 0,
-//   },
-//   visible: {
-//     pathLenght: 0,
-//     pathOffset: 1,
-//     opacity: 1,
-//     transition: {
-//       duration: 5,
-//       ease: "easeInOut",
-//     },
-//   },
-// }
+
 const Loading = () => {
   return (
     <LoadingBackground>
@@ -46,7 +72,7 @@ const Loading = () => {
         x="0px"
         y="0px"
         viewBox="106 -106 512 512"
-        enable-background="new 106 -106 512 512"
+        enableBackground="new 106 -106 512 512"
         space="preserve"
       >
         <polygon
@@ -57,11 +83,13 @@ const Loading = () => {
         <polygon fill="#282b31" points="541.2,-29.2 618,47.6 618,-29.2 " />
         <polygon fill="#324158" points="182.8,406 106,329.2 182.8,329.2 " />
         <polygon fill="#282b31" points="259.6,406 182.8,329.2 182.8,406 " />
+
         <path
-          fill="#0e57c5"
-          stroke="#0e57c5"
-          stroke-width="2.8346"
-          stroke-miterlimit="10"
+          fill="none"
+          className="dot"
+          stroke="#64ffda"
+          strokeWidth="4"
+          strokeMiterlimit="10"
           d="M369.4-12.2c1.2,0.2,2.6,0.6,3.8,1l0,0l0.6,0.2
 	l0.2,0.2h0.2c2,1,4,2,5.8,3.2l0,0h0.2l0.2,0.2l16.7-11.9c1.2-0.8,2.8-0.6,3.8,0.8l8.9,12.3c0.8,1.2,0.6,3-0.6,3.8L392.7,9.6
 	c0.4,2,0.8,4,1,6v0.2l0,0l0.2,0.2v0.2v0.2v0.2v0.2v0.2c0,1.6,0,3.2-0.2,4.6l18.8,8.5c1.4,0.6,2,2.4,1.4,3.8l-6.2,13.7
@@ -72,11 +100,13 @@ const Loading = () => {
 	c-1.4-0.6-2-2.2-1.4-3.6l6.4-13.9c0.6-1.4,2.2-2,3.6-1.4l18.5,8.5c1.6-1.6,3-3,4.8-4.2h0.2v-0.2h0.2l0,0l0,0c1.6-1,3-2,4.6-2.6
 	l-2-20.4c0-1.6,1-2.8,2.6-3l15.1-1.6c1.6,0,2.8,1,3,2.6L369.4-12.2z"
         />
+
         <path
-          fill="#0e57c5"
-          stroke="#0e57c5"
-          stroke-width="2.8346"
-          stroke-miterlimit="10"
+          fill="none"
+          className="symbol"
+          stroke="#64ffda"
+          strokeWidth="4"
+          strokeMiterlimit="10"
           d="M408.6,317.7L391.3,94.6l-48.2,0.2l-34.5,211.5
 	c-0.6,4.2,1,7.3,4.8,7.7l90.1,10.3C406.2,324.4,409,322.7,408.6,317.7L408.6,317.7z"
         />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,36 @@
 import React from "react"
 import { Link } from "gatsby"
-import Icon from "../components/Icon"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import Loading from "../components/loading/Loading"
+import { useTimeout } from "ahooks"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>HI Its mi</p>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <h1>NEW</h1>
+const IndexPage = () => {
+  const [isLoading, setIsLoading] = React.useState(true)
+
+  useTimeout(() => {
+    setIsLoading(false)
+  }, 4500)
+
+  return isLoading ? (
     <Loading />
-    <div>
-      <Icon />
-    </div>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+  ) : (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>HI Its mi</p>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <h1>NEW</h1>
+      <div></div>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
